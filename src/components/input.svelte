d34aae91fefd8d2e7c1e89db9d00a3b4fc4a3904
unknown <emmanueladesipe01@gmail.com>
2024-07-09 04:48:13 +0100

    built the Ui

---
+++ src/components/input.svelte
@@ -0,0 +1,20 @@
+<script lang='ts'>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher()
+
+    export let number: Number;
+    export let key: String;
+
+    const passVariable = (e) =>{
+        let allowed = ['1','2','3','4','5','6','7','8','9','0']
+        dispatch('date', {[key]:number}) //learn difference btw tis & spread operator: obj ={...obj, [key]:number}
+    }
+
+    
+</script>
+
+<div>
+    <p class='font-light uppercase'>{key}</p>
+    <input type='text' bind:value={number} on:input|preventDefault={e=>passVariable(e)}>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,64 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+    import Input from "../components/input.svelte";
+    import Icon from "../lib/assests/icon-arrow.svg?raw";
+
+    let day: Number = 16;
+    let month: Number = 4;
+    let year: Number = 2006;
+
+    let calculatedDay: Number;
+    let calculatedMonth: Number;
+    let calculatedYear: Number;
+
+    let inputs = {'day':day,'month':month,'year':year}
+    let keyp = 'day'
+    
+    let updateVariable = ( variable ) =>{
+        const [key, value] = Object.entries(variable)[0]
+        switch (key){
+            case 'day':
+                day = value
+                break
+            case 'month':
+                month = value
+                break
+            case 'year':
+                year = value
+                break
+        }
+    }
+
+    let calculateVariable = () =>{
+        calculatedDay = day
+        calculatedMonth = month
+        calculatedYear = year
+    }
+</script>
+
+<main class='container'>
+    <section class='wrapper'>
+        <div class='section-1'>
+            {#each Object.entries(inputs) as [key, variable], i}
+                <Input on:date={(event)=>updateVariable(event.detail)} {key} number={variable} />
+            {/each}
+        </div>
+
+        
+        <div class='bg-gray-300 my-5 h-[1px] relative'> <!--to move to tailwind-->
+            <!-- A11y: <div> with click handler must have an ARIA role -->
+            <div on:keydown on:click={()=>calculateVariable()} class='cursor-pointer rounded-full inline-block p-2 bg-black absolute mt-[-30px] right-0 top-[50%]'>
+                {@html Icon}
+            </div>
+        </div>
+
+        <div class='section-2'>
+            <p><span>{calculatedDay ? calculatedDay : '--'}</span> Days</p> 
+            <p><span>{calculatedMonth ? calculatedMonth : '--'}</span> Months</p>
+            <p><span>{calculatedYear ? calculatedYear : '--'}</span> Years </p>
+        </div>
+    </section>
+</main>
+
+<style>
+
+</style>
